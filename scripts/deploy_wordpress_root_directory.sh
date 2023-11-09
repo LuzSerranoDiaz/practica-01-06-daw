8ded3a916517669c3d887cc6c7a36b61ea960a99
LuzSerranoDiaz <diaz03luz@gmail.com>
2023-11-09 18:26:50 +0000

    aaa

--- scripts/deploy_wordpress_root_directory.sh
+++ scripts/deploy_wordpress_root_directory.sh
@@ -8,4 +8,21 @@ set -ex
 # ACtualizamos los paquetes del sistema
 # apt upgrade -y
 
-#borrar tmp wordpress
+#instalamos zip
+sudo apt install zip -y
+
+#instalamos tar
+sudo apt install tar
+
+#borrar versiones anteriores en tmp wordpress
+rm -rf /tmp/latest.tar.gz
+
+#poner wordpress en tmp
+wget http://wordpress.org/latest.tar.gz -P /tmp
+
+#descomprimimos el archivo gz
+gunzip /tmp/latest.tar.gz
+
+#descomprimimos el archivo tar
+tar -xzvf /tmp/latest.tar.gz -C /tmp
+
